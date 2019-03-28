fd696f13e24e4c40ae428cdbc03a065ca44c228e
Ruben Salas <rubsalas@hotmail.com>
2019-03-27 21:09:36 -0600

    Class Ficha

---
+++ Ficha.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ruben on 27/03/19.
+//
+
+#include "Ficha.h"
---
+++ Ficha.h
@@ -0,0 +1,14 @@
+//
+// Created by ruben on 27/03/19.
+//
+
+#ifndef SCRABBLE_FICHA_H
+#define SCRABBLE_FICHA_H
+
+
+class Ficha {
+
+};
+
+
+#endif //SCRABBLE_FICHA_H
